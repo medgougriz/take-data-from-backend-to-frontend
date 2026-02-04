4e32004560dbc06aa275406329c0db1e362069f5
Mohamed Gougriz <mohmadgougriz@gmail.com>
2026-02-04 20:09:46 +0100

    version 2

--- frontend/src/conponment/Nav.jsx
+++ frontend/src/conponment/Nav.jsx
@@ -2,7 +2,7 @@ export default function Nav({user}) {
     return(
         <nav >
             <ul style={{display:"flex",gap:"20px",listStyleType:"none"}}>
-                <li><a href={user}>Home</a></li>
+                <li><a href={user} target="_blank" rel="noopener noreferrer">Home</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/contact">Contact</a></li>
             </ul>
